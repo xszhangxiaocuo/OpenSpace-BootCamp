7d4c461a33df735cefde2433e7bf027a382466f6
xszxc <2112067692@qq.com>
2025-02-11 17:13:44 +0800

    day1:work1

--- day1/work1/main.go
+++ day1/work1/main.go
@@ -1,3 +1,9 @@
+/*
+实践 POW， 编写程序（编程语言不限）用自己的昵称 + nonce，不断修改nonce 进行 sha256 Hash 运算：
+
+直到满足 4 个 0 开头的哈希值，打印出花费的时间、Hash 的内容及Hash值。
+再次运算直到满足 5 个 0 开头的哈希值，打印出花费的时间、Hash 的内容及Hash值。
+*/
 package main
 
 import (
