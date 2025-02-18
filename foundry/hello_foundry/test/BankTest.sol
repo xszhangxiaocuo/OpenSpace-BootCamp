b4726537433c6b5aa8b0acaea1a55891d657481f
xszxc <2112067692@qq.com>
2025-02-18 21:19:56 +0800

    Bank合约测试

--- foundry/hello_foundry/test/BankTest.sol
+++ foundry/hello_foundry/test/BankTest.sol
@@ -16,19 +16,19 @@ contract BankTest is Test {
         uint256 depositAmount = 1 ether;
         vm.deal(user, 10 ether);
 
-        // 1. 检查存款前余额
+        // 检查存款前余额
         uint256 initialBalance = bank.balanceOf(user);
         assertEq(initialBalance, 0, "Initial balance should be 0");
 
-        // 2. 监听 Deposit 事件
+        // 监听 Deposit 事件
         vm.expectEmit(true, true, true, true);
         emit Bank.Deposit(user, depositAmount);
 
-        // 3. 执行存款交易
+        // 执行存款交易
         vm.prank(user); // 让 `user` 作为 `msg.sender` 进行调用
         bank.depositETH{value: depositAmount}();
 
-        // 4. 检查存款后余额
+        // 检查存款后余额
         uint256 newBalance = bank.balanceOf(user);
         assertEq(newBalance, depositAmount, "Balance should be updated correctly");
     }
