7b13d1d4b32182191795357f56c1940591c6bfb8
xszxc <2112067692@qq.com>
2025-03-16 20:08:02 +0800

    测试MyDex合约

--- foundry/my-uniswap-dex/test/MyDex.t.sol
+++ foundry/my-uniswap-dex/test/MyDex.t.sol
@@ -55,9 +55,9 @@ contract MyDexTest is Test {
     function setUp() public {
         vm.createSelectFork("http://127.0.0.1:8545");
 
-        address routerAddr = vm.envAddress("ROUTER");
-        address factoryAddr = vm.envAddress("FACTORY");
-        weth = vm.envAddress("WETH");
+        address routerAddr = 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0;
+        address factoryAddr = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
+        weth = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512;
 
         router = IUniswapV2Router02(routerAddr);
         factory = IUniswapV2Factory(factoryAddr);
